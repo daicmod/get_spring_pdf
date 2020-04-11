f125543417b0d4bcf6b34d6db5d0aedee9332084
daicmod <daicmod@gmail.com>
2020-04-12 01:41:42 +0900

    不要な行の削除 ファイル名の禁止文字除去部分をdfの段階でやるように変更。

--- get_springer_pdf.py
+++ get_springer_pdf.py
@@ -2,8 +2,6 @@ import pandas as pd
 import requests
 import time
 
-pd.set_option("display.max_colwidth", 100)
-
 df = pd.read_excel('./Free+English+textbooks.xlsx')
 df_URL = df['DOI URL'].astype(str)
 df_URL = df_URL.str.replace("http://doi.org/","HOGEHOGE")
@@ -11,20 +9,21 @@ df_URL = df_URL.str.replace('/','%2F')
 df_URL = df_URL.str.replace('HOGEHOGE','https://link.springer.com/content/pdf/')
 df_URL = df_URL + '.pdf'
 download_url_list = df_URL.values.tolist()
+
 df_TITLE = df['Book Title']
+df_TITLE = df_TITLE.str.replace('/', '')
+df_TITLE = df_TITLE.str.replace(':', '')
+df_TITLE = df_TITLE.str.replace('|', '')
+df_TITLE = df_TITLE.str.replace('"', '')
+df_TITLE = df_TITLE.str.replace('?', '')
+df_TITLE = df_TITLE.str.replace('>', '')
+df_TITLE = df_TITLE.str.replace('<', '')
+df_TITLE = df_TITLE.str.replace('\\', '')
 download_title_list = df_TITLE.values.tolist()
 
-i = 0
+
 for (download_url, download_title) in zip(download_url_list, download_title_list):
     title = str(download_title)
-    title = title.replace('/', '')
-    title = title.replace(':', '')
-    title = title.replace('|', '')
-    title = title.replace('"', '')
-    title = title.replace('?', '')
-    title = title.replace('<', '')
-    title = title.replace('>', '')
-    title = title.replace('\\', '')
     print('DOWNLOADING...: ' + title, end='\r')
     # 一秒スリープ
     time.sleep(1)
